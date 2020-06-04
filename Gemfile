ea9af04ab265d4750813842e1628890b133899f7
Rkebede <rutht.kebede@gmail.com>
2020-06-04 11:23:32 -0500

    installs gems

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'shotgun'
+gem 'activerecord'
+gem 'sinatra'
+gem 'rake'
+gem 'sqlite3'
+gem 'pry'
+gem 'require_all'
--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,5 @@ Bundler.require(:default, ENV[‘SINATRA_ENV’])
 	configure :development do 
 		set :database, {adapter: ’sqlite3’, database: db.database.sqlite3}
 	end 
+
+require_all 'app'
