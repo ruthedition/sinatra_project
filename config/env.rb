50c5cdec4eb79bc189f78cedfbee423574b1fbbe
Rkebede <rutht.kebede@gmail.com>
2020-06-15 11:10:29 -0500

    updates the spec.md

--- config/env.rb
+++ config/env.rb
@@ -8,5 +8,8 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/wines#{ENV['SINATRA_ENV']}.sqlite"
 )
+if ENV['SINATRA_ENV']== 'development'
+    require_relative '../secrets'
+end 
 
 require_all 'app'
